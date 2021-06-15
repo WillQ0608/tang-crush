d40c2b82762b32ad6d0f558599e8881395d55f6b
WillQ0608 <73808996+WillQ0608@users.noreply.github.com>
2021-06-16 03:07:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 const CONFIG = {
     introTitle: 'Linhh!!',
-    introDesc: `tôi đã phạm sai lầm
-    phá vỡ niềm tin em
-    giờ chưa nói được gì
-    time will tell`,
-    btnIntro: 'trễ rồi!',
+    introDesc: `Trễ lắm rồi, sao hôm nào cũng hại bản thân vậy`,
+    btnIntro: 'Em thích thức khuya đấy thì sao!',
     title: 'Em đã quên cho bản thân sống tích cực và ngủ đúng giờ >< ?',
     desc: 'Phải chăng em đã quen với việc hại bản thân tới 6h sáng r ngủ tới 1 giờ chiều ',
     btnYes: 'ok e đi ngủ nè, 15p cuối nha <3',
     btnNo: 'Tại em thích vậy đó !',
-    question:'Tại sao không đi ngủ sớm, k thương bản thân hả?',
-    btnReply: 'Gửi cho anh',
+    question:'Chịu đi ngủ sớm là ngoan đó, vậy mới tích cực được!',
+    btnReply: 'Cám ơn ha!',
     reply: 'Ngủ ngon nha',
     mess: 'Ngoan đó!',
     messDesc: 'Chúc em luôn vui.',
